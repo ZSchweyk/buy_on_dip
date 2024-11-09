b300349296ded6ef7e439f740f3e21f741448cea
ZSchweyk <zeynschweyk@dpengineering.org>
2024-11-08 23:28:34 -0500

    messing with share quantities lol... it works actually but is unrealistic, especially the doubling. Need to base number of shares on percent drop

--- main.py
+++ main.py
@@ -12,8 +12,13 @@ end_date = "2024-11-08"
 percent_drop_min = .05
 percent_gain_min = .05
 
+quantities = [2 ** i for i in range(0, 20)]
+
 #############################################
 
+
+def calc_mean_price(buy_prices: list, buy_quantities: list):
+    return numpy.dot(buy_prices, buy_quantities) / sum(buy_quantities)
 
 
 
@@ -24,17 +29,19 @@ dates_list = list(data.index.values)
 prev_price = close_list[0]
 buy_dates = []
 buy_prices = []
+buy_quantities = []
 for price, date in zip(close_list, dates_list):
     if len(buy_prices) != 0:
-        average_buy_price = numpy.mean(buy_prices)
+        average_buy_price = calc_mean_price(buy_prices, buy_quantities)
         percent_gain = (price - average_buy_price) / average_buy_price
         if percent_gain >= percent_gain_min:
             num_days = numpy.timedelta64(date - buy_dates[0], 'D').astype(int)
             annualized_return = (1 + percent_gain) ** (365/num_days) - 1
-            print(f"Investment gained {round(percent_gain * 100, 2)}% on {str(date)[:10]}. Span of {num_days} days. Sold {len(buy_prices)} shares at ${round(price, 2)}")
+            print(f"Investment gained {round(percent_gain * 100, 2)}% on {str(date)[:10]}. Span of {num_days} days. Sold {sum(buy_quantities)} shares at ${round(price, 2)}")
             print(f"Annualized return: {round(annualized_return * 100, 2)}%\n")
             buy_prices = []
             buy_dates = []
+            buy_quantities = []
 
     
     percent_drop = (price - prev_price) / prev_price
@@ -43,8 +50,8 @@ for price, date in zip(close_list, dates_list):
     if (len(buy_prices) == 0 and percent_drop <= -percent_drop_min):
         msg = f"{ticker} dropped {round(percent_drop * 100, 2)}% on {str(date)[:10]}"
         condition_satisfied = True
-    elif len(buy_prices) != 0 and price <= numpy.mean(buy_prices) * (1-percent_drop_min):
-        msg = f"{ticker} is below the mean by {round((1 - price/numpy.mean(buy_prices)) * 100, 2)}%>={round(percent_drop_min * 100, 2)}% on {str(date)[:10]}"
+    elif len(buy_prices) != 0 and price <= calc_mean_price(buy_prices, buy_quantities) * (1-percent_drop_min):
+        msg = f"{ticker} is below the mean by {round((1 - price/calc_mean_price(buy_prices, buy_quantities)) * 100, 2)}%>={round(percent_drop_min * 100, 2)}% on {str(date)[:10]}"
         condition_satisfied = True
     
     if condition_satisfied:
@@ -52,7 +59,8 @@ for price, date in zip(close_list, dates_list):
         # Of course, buy if the stock hasn't yet dipped at all or if this is a new segment.
         buy_prices.append(price)
         buy_dates.append(date)
-        print(f"{msg}. Bought 1 share at ${round(buy_prices[-1], 2)}. Average price at ${round(numpy.mean(buy_prices), 2)}.")
+        buy_quantities.append(quantities[len(buy_quantities)])
+        print(f"{msg}. Bought {buy_quantities[-1]} shares at ${round(buy_prices[-1], 2)}. Average price at ${round(calc_mean_price(buy_prices, buy_quantities), 2)}.")
 
     prev_price = price
 
